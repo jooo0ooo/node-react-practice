5e4fbb4e9602b5c80e713ede96cf307469eda812
Jiwoo Jung <pingrae@me.com>
2021-02-18 22:42:53 +0900

    mongodb connect

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb connecting info...', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
