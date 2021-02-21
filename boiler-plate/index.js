4b0c148632d1d2189c24aee95d8476f5b49d1fd8
Jiwoo Jung <pingrae@me.com>
2021-02-21 17:08:57 +0900

    set confidential info protection

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -4,6 +4,8 @@ const port = 5000
 const bodyParser = require('body-parser')
 const {User} = require("./models/User")
 
+const config = require('./config/key')
+
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -11,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb connecting info...', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
