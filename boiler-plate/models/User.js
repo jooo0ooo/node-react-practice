5bdae7c9805dbb2a47bb10e53f321b9071611280
Jiwoo Jung <pingrae@me.com>
2021-02-21 17:32:25 +0900

    encrypt pwd with bcrypt

--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 
 const userSchema = mongoose.Schema({
@@ -29,6 +31,23 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next) {
+    let user = this
+
+    if (user.isModified('password')) {
+        //encrypt password
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
     }
 })
 
