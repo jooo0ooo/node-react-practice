db82ec01e389f908c522946c91795e86fd1ce195
Jiwoo Jung <pingrae@me.com>
2021-03-04 10:15:07 +0900

    add upload function with multer

--- youtube-clone/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ youtube-clone/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {Typography, Button, Form, message, Input, Icon} from 'antd'
 import Dropzone from 'react-dropzone'
+import Axios from 'axios'
 const { TextArea } = Input
 const { Title } = Typography
 
@@ -39,6 +40,23 @@ function VideoUploadPage() {
         setCategory(e.currentTarget.value)
     }
 
+    const onDrop = (files) => {
+        let formData = new FormData
+        const config = {
+            header: {'content-type': 'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+
+        Axios.post('/api/video/uploadfiles', formData, config)
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data)
+                } else {
+                    alert('Video Upload Failed')
+                }
+            })
+    }
+
     return (
         <div style={{maxWidth: "700px", margin: "2rem auto"}}>
             <div style={{textAlign: "center", marginBottom: "2rem"}}>
@@ -48,9 +66,9 @@ function VideoUploadPage() {
                 <div style={{display: "flex", justifyContent: "space-between"}}>
                     {/* Drop Zone */}
                     <Dropzone
-                        onDrop
-                        multiple
-                        maxSize
+                        onDrop={onDrop}
+                        multiple={false}
+                        maxSize={100000000}
                     >
                         {({getRootProps, getInputProps}) => (
                             <div style={{width: "300px", height: "240px", border: "1px solid lightgray", display: "flex",
